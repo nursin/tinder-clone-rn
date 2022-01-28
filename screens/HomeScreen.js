11663ab9074408bcb44c568dbac0f2dc17b002fa
Bobby Keel <bobbykeel76@gmail.com>
2022-01-28 12:20:30 -0600

    home, chat, login, hooks, stacknavigator, ...

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+  return (
+    <View>
+      <Text>I am the home screen</Text>
+      <Button title="Go to Chat Screen" onPress={() => navigation.navigate("Chat")}/>
+    </View>
+  );
+};
+
+export default HomeScreen;
