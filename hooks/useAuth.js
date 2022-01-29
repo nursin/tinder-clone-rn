73402e903f2b1f5c698463949b60d7d01fa07afe
Bobby Keel <bobbykeel76@gmail.com>
2022-01-29 02:24:33 -0600

    auth

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,12 +1,18 @@
 import { View, Text } from 'react-native';
 import React, { createContext, useContext } from 'react';
+import * as Google from "expo-google-app-auth";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
+
+    const signInWithGoogle = async () => {
+        await Google.logInAsync()
+    }
+
     return (
         <AuthContext.Provider value={{
-            user: "Bobby",
+            user: null,
         }}
         >
             {children}
